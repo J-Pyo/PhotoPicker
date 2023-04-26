e078193a143a0d7ce87dfb311ff462b1c82f8023
Jeongpyo <jeongpyo6223@gmail.com>
2023-04-26 17:20:56 +0900

    이미지 선택시 ViewController중앙에 뜨는 로직 생성

--- Pixo/ViewController.swift
+++ Pixo/ViewController.swift
@@ -6,21 +6,61 @@
 //
 
 import UIKit
+import Photos
 
 class ViewController: UIViewController {
 
     let popupPhotoPicker = PhotoPickerCollectionViewController()
-        
+    let screenWidth = UIScreen.main.bounds.size.width
+    var imageView = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        popupPhotoPicker.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
     
         self.present(popupPhotoPicker, animated: true, completion: nil);
+    }
+    func showImage(asset: PHAsset){
+        let imageManager = PHImageManager()
+        let options = PHImageRequestOptions()
+        options.isSynchronous = false
+        options.deliveryMode = PHImageRequestOptionsDeliveryMode.highQualityFormat
+        options.isNetworkAccessAllowed = true
         
+//        options.progressHandler = {  (progress, error, stop, info) in
+//                print("progress: \(progress)")
+//            }
+//
+        let frame = CGRect(x: (screenWidth / 2) - (screenWidth / 2), y: (UIScreen.main.bounds.height / 2) - (screenWidth / 2), width: screenWidth, height: screenWidth)
+        
+        self.imageView.frame = frame
+        imageManager.requestImage(for: asset, targetSize: PHImageManagerMaximumSize, contentMode: .aspectFill, options: options) { image, _ in
+            guard let image = image else{
+                print("nill Image")
+                return
+            }
+            self.imageView.image = image
+
+            self.imageView.contentMode = .scaleAspectFit
+        }
+        
+        self.view.addSubview(imageView)
     }
 }
-
+extension ViewController: PhotoPickerViewControllerDelegate{
+    func didUpdateState(to state: PhotoPickerViewControllerState) {
+        switch state {
+        case .getAsset:
+            print("get Asset")
+            guard let asset = popupPhotoPicker.selectedAsset else{
+                return
+            }
+            showImage(asset: asset)
+        case .error(let reason):
+            print("error : \(reason)")
+        }
+    }
+}
