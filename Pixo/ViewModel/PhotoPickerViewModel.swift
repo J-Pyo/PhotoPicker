5c494918530a0bc4dbfe350d516c5ef9a5fb1acb
Jeongpyo <jeongpyo6223@gmail.com>
2023-04-26 14:11:49 +0900

    ViewModel 로직 작성 Delegate 패턴을 활용해서 image를 받았을때 ViewController에  didSet을 통해 알려줌

---
+++ Pixo/ViewModel/PhotoPickerViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  PhotoPickerViewModel.swift
+//  Pixo
+//
+//  Created by 홍정표 on 2023/04/25.
+//
+
+import Foundation
+import Photos
+import UIKit
+
+protocol PhotoPickerViewModelDelegate: AnyObject{
+    func didUpdateState(to state: PhotoPickerViewModelState)
+}
+enum PhotoPickerViewModelState{
+    //이미지 불러오기 완료
+    case gotPhoto
+    //Error
+    case error(reason: String)
+}
+class PhotoPickerViewModel{
+    weak var delegate: PhotoPickerViewModelDelegate?
+
+    var allPhoto: PHFetchResult<PHAsset>?{
+        didSet{
+            self.delegate?.didUpdateState(to: .gotPhoto)
+        }
+    }
+    let imageManager = PHImageManager()
+    
+    func fetchAllPhotos(){
+        let options = PHFetchOptions()
+        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+        
+        self.allPhoto = PHAsset.fetchAssets(with: nil)
+        
+        guard let allPhoto = allPhoto else{
+            return
+        }
+        print(allPhoto.count)
+    }
+    
+    func requestImage(){
+        
+    }
+    
+}
