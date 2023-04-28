d2a3da9d664c7fc51b865205da688a51cf727f78
Jeongpyo <jeongpyo6223@gmail.com>
2023-04-28 14:44:16 +0900

    svgScrollView에 Delegate 추가

--- Pixo/View/SVGScrollView.swift
+++ Pixo/View/SVGScrollView.swift
@@ -6,6 +6,15 @@
 //
 
 import UIKit
+protocol SVGScrollViewDelegate: AnyObject{
+    func didUpdateState(to state: SVGScrollViewState)
+}
+enum SVGScrollViewState{
+    //ViewController 이미지 보내기
+    case sendImageInfo
+    //Error
+    case error(reason: String)
+}
 class SVGScrollView: UIScrollView {
     var svgImageName = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014"]
     //이미지를 담을 View
@@ -16,27 +25,51 @@ class SVGScrollView: UIScrollView {
         return stackView
         }()
     
-    var imageBtn = UIButton()
+    
+    weak var svgDelegate: SVGScrollViewDelegate?
+    
+    var sendedImg: UIImage?{
+        didSet{
+            self.svgDelegate?.didUpdateState(to: .sendImageInfo)
+        }
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.addSubview(stackView)
-        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    func addSvgImageToStackView(){
+    func addSvgImageToStackView(target: ViewController){
+        
         for name in svgImageName {
             //stackView 이미지를 표현할 이미지뷰
-            let imageView = UIImageView()
+            var imageView = UIImageView()
             imageView.image = UIImage(named: name)
             imageView.translatesAutoresizingMaskIntoConstraints = true
             imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
             imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
             imageView.contentMode = .scaleAspectFit
+            
+            
+            imageView.tag = 1
+            //클릭 가능하도록 설정
+            imageView.isUserInteractionEnabled = true
+            //제쳐스 추가
+            let gesture = MyTapGesture(target: self, action: #selector(self.viewTapped))
+            gesture.image = UIImage(named: name) ?? UIImage()
+            imageView.addGestureRecognizer(gesture)
+    
             stackView.addArrangedSubview(imageView)
         }
     }
+    @objc func viewTapped(_ sender: MyTapGesture) {
+        let image = sender.image
+        sendedImg = image
+    }
 }
-
+class MyTapGesture: UITapGestureRecognizer {
+    var image = UIImage()
+}
